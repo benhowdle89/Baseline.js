60522e8a623b8b413d8a9b51f749c6f7936430d0
Ben Howdle <benhowdle89@gmail.com>
2012-06-21 22:53:08 +0100

    Converted to vanilla ice cream JS

---
+++ baselinejs.js
@@ -0,0 +1,45 @@
+/*
+* BaselineJS.js 1.0
+*
+* Copyright 2012, Ben Howdle http://twostepmedia.co.uk
+* Released under the WTFPL license
+* http://sam.zoy.org/wtfpl/
+*
+* Date: Thu June 21 20:58 2012 GMT
+*/
+
+var baseline = function(){
+
+	var tall, newHeight, target, imgl, cur, images = [];
+
+	return {
+
+		init: function(selector, target){
+			if(selector === undefined || target === undefined) return false;
+			this.images = document.querySelectorAll(selector);
+			this.target = target;
+			this.setbase(this.images);
+			var me = this;
+			window.onresize = function() {
+				me.setbase(me.images);
+			};
+		},
+
+		setbase: function(imgs){
+			this.imgl = imgs.length;
+			if(this.imgl !== 0){
+				while(this.imgl--){
+					this.cur = imgs[this.imgl];
+					this.cur.removeAttribute("style");
+					this.tall = this.cur.offsetHeight;
+					this.newHeight = Math.floor(this.tall / this.target) * this.target;
+					this.cur.style.maxHeight = this.newHeight + 'px';
+				}
+			} else {
+				return false;
+			}
+		}
+
+	};
+
+}();
